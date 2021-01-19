cdb43b805342f0a1595c54121b69f0bfb5eeeca2
Gabriele <sucre.91@hotmail.it>
2021-01-19 11:46:14 +0100

    Improve visualization

--- igwn-data-checker-visualizer.py
+++ igwn-data-checker-visualizer.py
@@ -9,46 +9,67 @@ import matplotlib.pyplot as plt
 def main(json_path="output-cnaf.json"):
     checksum_results = []
     user_time_results = []
+    sys_MBps_results = []
     sys_time_results = []
     real_time_results = []
+    file_sizes = []
     data = json.load(open(os.path.abspath(json_path)))
-    for path, results in data.items():
+    for path, data in data.items():
         avg_user_time = 0
         avg_sys_time = 0
         avg_real_time = 0
+        size = data["size"]
+        file_sizes.append(size/2**20)
+        results = data["results"]
+        print(results)
         for r in results:
             checksum_results.append(1 if r["checksum_status"] else 0)
             avg_user_time += r["timer"]["user"]
             avg_sys_time += r["timer"]["sys"]
             real_time = r["timer"]["real"].split(":")
             avg_real_time += float(real_time[0])*60+float(real_time[1])
-        user_time_results.append(avg_user_time/len(results))
-        sys_time_results.append(avg_sys_time/len(results))
-        real_time_results.append(avg_real_time/len(results))
+        user_time_results.append((avg_user_time/len(results)))
+        sys_time_results.append((avg_sys_time/len(results)))
+        sys_MBps_results.append((size/2**20)/(avg_sys_time/len(results)))
+        real_time_results.append((avg_real_time/len(results)))
 
-    fig, axes = plt.subplots(nrows=2, ncols=2)
-    ax0, ax1, ax2, ax3 = axes.flatten()
+    fig, axes = plt.subplots(nrows=3, ncols=2)
+    ax = axes.flatten()
 
-    ax0.hist(checksum_results, 2, histtype='bar', color='blue')
-    plt.sca(ax0)
+    print(checksum_results)
+    n_checksums = len(checksum_results)
+
+
+    ax[0].hist(checksum_results, 2, histtype='bar', weights=[1/n_checksums*100] * n_checksums, color='blue')
+    plt.sca(ax[0])
     plt.xticks([.75, 1.25], ["wrong", "correct"])
-    ax0.set_title("Checksum verification distribution")
-    ax0.set_ylabel("percent [%]")
+    ax[0].set_title("Checksum verification distribution")
+    ax[0].set_ylabel("percent [%]")
 
-    ax1.hist(user_time_results, 100, histtype='bar', color='lime')
-    ax1.set_title("File access time (user)")
-    ax1.set_xlabel("seconds [s]")
-    ax1.set_ylabel("counts")
+    ax[1].hist(user_time_results, 100, histtype='bar', color='lime')
+    ax[1].set_title("File access time (user)")
+    ax[1].set_xlabel("seconds [s]")
+    ax[1].set_ylabel("counts")
+
+    ax[2].hist(sys_time_results, 100, histtype='bar', color='orange')
+    ax[2].set_title("File access time (sys)")
+    ax[2].set_xlabel("seconds [s]")
+    ax[2].set_ylabel("counts")
     
-    ax2.hist(sys_time_results, 100, histtype='bar', color='orange')
-    ax2.set_title("File access time (sys)")
-    ax2.set_xlabel("seconds [s]")
-    ax2.set_ylabel("counts")
-    
-    ax3.hist(real_time_results, 100, histtype='bar', color='red')
-    ax3.set_title("File access time (real)")
-    ax3.set_xlabel("seconds [s]")
-    ax3.set_ylabel("counts")
+    ax[3].hist(real_time_results, 100, histtype='bar', color='red')
+    ax[3].set_title("File access time (real)")
+    ax[3].set_xlabel("seconds [s]")
+    ax[3].set_ylabel("counts")
+
+    ax[4].hist(file_sizes, 100, histtype='bar', color='deepskyblue')
+    ax[4].set_title("File sizes distribution")
+    ax[4].set_xlabel("MB")
+    ax[4].set_ylabel("counts")
+
+    ax[5].hist(sys_MBps_results, 100, histtype='bar', color='purple')
+    ax[5].set_title("File transfer speed")
+    ax[5].set_xlabel("Bandwidth [MB/s]")
+    ax[5].set_ylabel("counts")
     
     fig.tight_layout()
     plt.show()
