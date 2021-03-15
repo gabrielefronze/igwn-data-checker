c5e567d7d377e4ed31f37a02c7eb5ef1431a0dcf
Gabriele <sucre.91@hotmail.it>
2021-03-15 18:01:18 +0100

    Improved output at runtime

--- bulk-visualizer.py
+++ bulk-visualizer.py
@@ -8,13 +8,18 @@ def main():
     root_path = os.path.abspath(os.path.dirname(__file__))
     path = os.path.join(root_path, "output")
     for cc in os.listdir(path):
+        print("\n\n=================================================================================================================================")
+        print("Handling {}".format(cc))
+        print("=================================================================================================================================")
         cc_dir = os.path.join(path, cc)
         for date in os.listdir(cc_dir):
+            print("\tDate {}".format(date))
             date_path = os.path.join(cc_dir, date)
             for time in os.listdir(date_path):
+                print("\t\tTime {}".format(time))
                 time_path = os.path.join(date_path, time)
                 if os.path.isfile(os.path.join(time_path, "output.json")):
-                    print("Output found.")
+                    print("\t\tOutput found.")
                     os.chdir(time_path)
                     vis = subprocess.run([os.path.join(root_path, "igwn-data-checker-visualizer.py"),
                                          "./output.json", 
@@ -22,15 +27,15 @@ def main():
                                          "-n",
                                          "-s"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     if vis.stderr:
-                        print("Errors occurred.")
+                        print("\t\tErrors occurred.")
                         print(vis.stderr.decode("utf-8"))
                     else:
                         os.rename("./output.json", "./output.processed.json")
                     os.chdir(root_path)
                 elif os.path.isfile(os.path.join(time_path, "output.processed.json")):
-                    print("Output already processed.")
+                    print("\t\tOutput already processed.")
                 else:
-                    print("No output found.")
+                    print("\t\tNo output found.")
 
 if __name__ == "__main__":
     main()
